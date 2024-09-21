e7e9815bcc28534615e1781b9d2db5aa713b874c
Lucasdiaz-dev <lucasdiazemanuel123@icloud.com>
2024-09-21 03:10:07 -0300

    Scoreboard v0.1 - via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let scoreh = 0
+let scoreg = 0
+let scoreGuest = document.getElementById("points-guest");
+
+let scoreHome = document.getElementById("points-home")
+
+function incrementh1() {
+    scoreh += 1
+    scoreHome.textContent = scoreh
+}
+
+function incrementh2() {
+    scoreh += 2
+    scoreHome.textContent = scoreh
+}
+
+function incrementh3() {
+    scoreh += 3
+    scoreHome.textContent = scoreh
+}
+
+function incrementg1() {
+    scoreg += 1
+    scoreGuest.textContent = scoreg
+}
+
+function incrementg2() {
+    scoreg += 2
+    scoreGuest.textContent = scoreg
+}
+
+function incrementg3() {
+    scoreg += 3
+    scoreGuest.textContent = scoreg
+}
